40352349273ee00472ebf2724b0bd07da426bf95
soboj-paul <sobojpaul.me@gmail.com>
2026-07-17 19:44:36 +0600

    season practice complete

---
+++ p-11.js
@@ -0,0 +1,10 @@
+let month = 4;
+if(month>=1 && month<=4){
+    console.log('this season is summer');
+}
+if(month>=5 && month<=8){
+    console.log('this season is monsoon');
+}
+if(month>=9 && month<=12){
+    console.log('this season is winter');
+}
