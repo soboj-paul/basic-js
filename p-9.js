92499123c08286e066664d1e72f283d9dbe5c20a
soboj-paul <sobojpaul.me@gmail.com>
2026-07-17 18:44:43 +0600

    grade points pactics complete

---
+++ p-9.js
@@ -0,0 +1,25 @@
+let marks = 70;
+if(marks >= 80){
+    console.log('Your grade is: A+');
+}
+else if(marks >= 70){
+    console.log('Your grade is: A');
+}
+else if(marks >= 65){
+    console.log('Your grade is: A-');
+}
+else if(marks >= 60){
+    console.log('Your grade is: B');
+}
+else if(marks >= 55){
+    console.log('Your grade is: C');
+}
+else if(marks >= 50){
+    console.log('Your grade is: D');
+}
+else if(marks >= 40){
+    console.log('Your grade is: E');
+}
+else if(marks <= 40){
+    console.log('Your grade is: F');
+}
