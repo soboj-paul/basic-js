64689996a7b8dff0b642710cac0783943a5515f2
soboj-paul <sobojpaul.me@gmail.com>
2026-07-23 13:33:07 +0600

    array practics start

---
+++ array/p1.js
@@ -0,0 +1,7 @@
+const number = [20, 12, 14, 54, 45, 24];
+console.log(number)
+console.log(number.length) //  see array length
+console.log(number[1]) // see index 1 num
+console.log(number[number.length-2]) // see index 2 num
+number[1] = 20; //change index 1 num
+console.log(number)
---
+++ array/p2.js
@@ -0,0 +1,7 @@
+const number = [20, 12, 14, 54, 45, 24];
+console.log(number);
+const add = number.push(100);
+console.log(number);
+const dalete = number.pop();
+console.log(dalete);
+console.log(number);
